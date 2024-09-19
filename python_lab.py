1813edf4f9e72c9c0374bedb48560966aaaf6d02
Hussein Tarhini <husseintarhini168@gmail.com>
2024-09-19 11:49:10 +0200

    last version of the first part

--- python_lab.py
+++ python_lab.py
@@ -155,4 +155,85 @@ assert backward_string("123456789") == "987654321"
 print("The mission is done! Click 'Check Solution' to earn rewards!")
 
 #ex 11 Just Fizz!
+def checkio(num: int) -> str:
+    if num % 3==0:
+        return "Fizz"
+    else:
+        return str(num)
+
+
+print("Example:")
+print(checkio(3))
+
+# These "asserts" are used for self-checking
+assert checkio(15) == "Fizz"
+assert checkio(6) == "Fizz"
+assert checkio(10) == "10"
+assert checkio(7) == "7"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
+
+#ex 12 First Word (simplified)
+
+def first_word(text: str) -> str:
+   word=text.split(' ')
+   return word[0]
+     
+print("Example:")
+print(first_word("Hello world"))
+
+# These "asserts" are used for self-checking
+assert first_word("Hello world") == "Hello"
+assert first_word("a word") == "a"
+assert first_word("greeting from CheckiO Planet") == "greeting"
+assert first_word("hi") == "hi"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
+
+
+#ex 13 Number Length
+
+def number_length(value: int) -> int:
+    value1=str(value)
+    return len(value1)
+
+
+print("Example:")
+print(number_length(10))
+
+# These "asserts" are used for self-checking
+assert number_length(10) == 2
+assert number_length(0) == 1
+assert number_length(4) == 1
+assert number_length(44) == 2
+
+print("The mission is done! Click 'Check' to earn cool rewards!")
+
+
+#ex14 Fizz Buzz
+# Taken from mission Just Fizz!
+
+def checkio(num: int) -> str:
+    if num%3==0 and num%5==0:
+        return "Fizz Buzz"
+    elif num % 3==0:
+        return "Fizz"
+    elif num %5==0:
+        return "Buzz"
+    
+    else:
+        return str(num)
+
+
+print("Example:")
+print(checkio(15))
+
+# These "asserts" are used for self-checking
+assert checkio(15) == "Fizz Buzz"
+assert checkio(6) == "Fizz"
+assert checkio(10) == "Buzz"
+assert checkio(7) == "7"
+
+print("The mission is done! Click 'Check Solution' to earn rewards!")
+
 
